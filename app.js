d5ec721aa19e3b3723b078a6f86d37a00705e01e
USMAN SIDDIQUI <121655186+Usmansiddiquu@users.noreply.github.com>
2023-03-11 16:05:29 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+//Task1
+// var name1 = prompt("Enter Player1 Name");
+// var name2 = prompt("Enter Player2 name");
+// var random1 = Math.random();
+// random1 = random1*1;
+// random2 = Math.round(random1);
+// if(random2 == 0){
+//     alert(name1 + " is win")
+// }
+// else(alert(name2 + " is win"))
+//     console.log(random2);
+
+
+// Task2
+// var random1 = Math.random();
+// random1 = random1*6;
+// random2 = Math.ceil(random1);
+// alert(random2);
+
+// Task3
+// var pass = "";
+// random1 =("01n");
+// random2 = random1*10;
+// random2 = random2.round();
+// alert(random2);
+// for(var i= 0; i<random1.length;i++){
+//     var char = (Math.random()*random1.length);
+//     pass = random1.charAt(char)
+// }
+// console.log(pass)
