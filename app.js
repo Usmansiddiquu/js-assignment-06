228af0231d9a7bf66a2496d0808c02806dad576c
USMAN SIDDIQUI <121655186+Usmansiddiquu@users.noreply.github.com>
2023-03-11 16:29:04 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+// var firstname = prompt("enter your firstname")
+// var lastname = prompt("enter your lastname")
+
+// alert("Hello " + firstname +" "+ lastname)
+
+// question 2
+
+// var model = prompt("write your favorite mobile phone model.")
+// console.log("length of string is " + model.length)
+
+// question 3
+
+// var country = "Pakistani"
+// if 
 
