014e153b333b6caea1fcdd88e90214a5d6b8bcf4
USMAN SIDDIQUI <121655186+Usmansiddiquu@users.noreply.github.com>
2023-05-28 12:22:49 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// question 1
+
 // var firstname = prompt("enter your firstname")
 // var lastname = prompt("enter your lastname")
 
@@ -11,5 +13,62 @@
 // question 3
 
 // var country = "Pakistani"
-// if 
+// console.log(country.indexOf("n"));
 
+// question 4
+
+// var word = "Hello world";
+// console.log(word.lastIndexOf("l"))
+
+// question 5
+
+// var word = "Pakistani";
+// console.log(word.charAt(3))
+
+// question 6
+
+// var firstname = prompt("enter your firstname")
+// var lastname = prompt("enter your lastname")
+
+// alert("Hello " + firstname +" "+ lastname)
+
+// question 7
+
+// var word = "Hyderabad";
+
+// var word2= word.replace("Hyder", "Islam")
+// console.log(word2)
+
+//question 8
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// word2 = message.replaceAll("and", "&")
+// console.log(word2)
+
+//question 9
+
+// var string ="472"
+// var num = Number(string)
+
+// console.log("Value: " + string)
+// console.log("Type of: " + typeof(string))
+
+
+// console.log("Value: " + num)
+// console.log("Type of: " + typeof(num))
+
+//question 10
+
+// var input = prompt("Enter your value")
+// for(var i =0; i<input.length; i++){
+//     var word = (input.charAt(i))
+//    var word2 = word.toUpperCase()
+
+// }
+// var arr =["shhds", "xdcuds"]
+// var word3 = arr.join("");
+// console.log(word3)
+// var word = "i";
+// // var word2 = word.toUpperCase();
+
+// console.log(word.toUpperCase)
